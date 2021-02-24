de8f5963f2b2142f0007b9fd9384c1ff9bad2e67
srportto <ceeh1100@gmail.com>
2021-02-24 00:11:46 -0300

    add models e trava para nao repetir hora agd

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -3,24 +3,41 @@
 */
 
 import {Router} from 'express';
-import { v4 as uuid } from 'uuid';
+// parseISO    ->  converte string para o formato date nativo do javascrip
+// startOfHour ->  start o datetime enviado na hora, zerando os minutos, seguns, etc
+import {startOfHour, parseISO, isEqual} from 'date-fns';
+import Appointment  from '../models/Appointment';
+
+
+
 
 const appointtmentsRouter = Router();
 
-const appointments = [];
+
+
+const appointments: Appointment [] = [];
 
 
 appointtmentsRouter.post('/', (request, response) => {
-  const {provider, date} =request.body;
-
   //provider = barbeador
   //date = data
+  const {provider, date} =request.body;
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date,
+  const parsedDate = startOfHour(parseISO(date));
+
+  console.log(parsedDate);
+  console.log(appointments);
+
+  const findAppointmentInSamedate = appointments.find(appointment => isEqual(parsedDate, appointment.date));
+
+  if (findAppointmentInSamedate){
+    return response.status(400).json({"message": "This appointment is already booked"});
   }
+
+
+  const appointment = new Appointment(provider,parsedDate);
+
+
 
   appointments.push(appointment);
 
